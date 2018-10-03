56ae2c5dd9f5a19bffe91b9fddb209620d51221c
Robby <robby@k-state.edu>
2018-10-03 14:58:30 -0500

    Added Tipe as part of CI tests.

--- test.sh
+++ test.sh
@@ -4,9 +4,10 @@ rm -fR mill-standalone out
 curl -Lo mill-standalone http://files.sireum.org/mill-standalone
 chmod +x mill-standalone
 git submodule update --init --recursive --remote
-$SCRIPT_HOME/mill-standalone -i all \
+$SCRIPT_HOME/mill-standalone cli.assembly
+out/cli/assembly/dest/out.jar slang tipe --verbose -r -s runtime/library:slang:tools:alir:cli
+$SCRIPT_HOME/mill-standalone all \
   cli.tests.compile \
-  cli.assembly \
   runtime.library.shared.tests.test \
   runtime.library.jvm.tests.test \
   slang.parser.shared.tests.test \
