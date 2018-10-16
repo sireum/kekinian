ec35da74cf9a19a1db03d700806d64fcb16118c7
Robby <robby@ksu.edu>
2018-10-16 08:57:47 -0500

    Tweaked test.sh.

--- test.sh
+++ test.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -e
 export SCRIPT_HOME=$( cd "$( dirname "$0" )" &> /dev/null && pwd )
+cd ${SCRIPT_HOME}
 rm -fR mill-standalone out
 curl -Lo mill-standalone http://files.sireum.org/mill-standalone
 chmod +x mill-standalone
 git submodule update --init --recursive --remote
-$SCRIPT_HOME/mill-standalone cli.assembly
+./mill-standalone cli.assembly
 out/cli/assembly/dest/out.jar slang tipe --verbose -r -s runtime/library:slang:tools:alir:cli
-$SCRIPT_HOME/mill-standalone all \
+./mill-standalone all \
   cli.tests.compile \
   runtime.library.shared.tests.test \
   runtime.library.jvm.tests.test \
