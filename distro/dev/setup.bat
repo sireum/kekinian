401fa3cac1d779bbe39912d7a82a20fbac27202d
Robby <robby@k-state.edu>
2019-01-25 09:15:23 -0600

    Updated distro setup scripts. [skip ci]

--- distro/dev/setup.bat
+++ distro/dev/setup.bat
@@ -1,16 +1,23 @@
 @echo off
 set INSTALL_PATH="%USERPROFILE%\Applications"
-choice /T 5 /C YN /D N /N /M "Customize where Sireum-dev should be installed under (defauts to %INSTALL_PATH%) [Y/N]? "
+choice /T 5 /C YN /D N /N /M "Customize where Sireum-dev should be installed under (defaults to %INSTALL_PATH%) [Y/N]? "
 if errorlevel 2 goto :install
 set /p INSTALL_PATH="Enter the directory path [Press Enter to use the default]: "
 :install
+echo Installing in %INSTALL_PATH% ...
 md %INSTALL_PATH% 1> nul 2>&1
 rd /S /Q %INSTALL_PATH%\Sireum-dev 2>nul
 move %~dp0Sireum-dev %INSTALL_PATH% 1> nul 2>&1
+if not exist %~dp0Sireum-dev\ goto failed
+:success
 echo Sireum-dev IVE can now be launched by running %INSTALL_PATH%\Sireum-dev\bin\win\idea\bin\idea64.exe
 echo Java Development Kit (JDK) is available at %INSTALL_PATH%\Sireum-dev\bin\win\java
 echo Scala is available at %INSTALL_PATH%\Sireum-dev\bin\scala
 echo Mill can be found at %INSTALL_PATH%\Sireum-dev\bin\mill.bat
+goto end
+:failed
+echo Installation failed
+:end
 echo Press any key to exit ...
 pause >nul
-(goto) 2>nul & del "%~f0"
+REM (goto) 2>nul & del "%~f0"
--- distro/release/setup.bat
+++ distro/release/setup.bat
@@ -1,16 +1,23 @@
 @echo off
 set INSTALL_PATH="%USERPROFILE%\Applications"
-choice /T 5 /C YN /D N /N /M "Customize where Sireum should be installed under (defauts to %INSTALL_PATH%) [Y/N]? "
+choice /T 5 /C YN /D N /N /M "Customize where Sireum should be installed under (defaults to %INSTALL_PATH%) [Y/N]? "
 if errorlevel 2 goto :install
 set /p INSTALL_PATH="Enter the directory path [Press Enter to use the default]: "
 :install
+echo Installing in %INSTALL_PATH% ...
 md %INSTALL_PATH% 1> nul 2>&1
 rd /S /Q %INSTALL_PATH%\Sireum 2>nul
 move %~dp0Sireum %INSTALL_PATH% 1> nul 2>&1
+if not exist %~dp0Sireum\ goto failed
+:success
 echo Sireum IVE can now be launched by running %INSTALL_PATH%\Sireum\bin\win\idea\bin\idea64.exe
 echo Java Development Kit (JDK) is available at %INSTALL_PATH%\Sireum\bin\win\java
 echo Scala is available at %INSTALL_PATH%\Sireum\bin\scala
 echo Mill can be found at %INSTALL_PATH%\Sireum\bin\mill.bat
+goto end
+:failed
+echo Installation failed
+:end
 echo Press any key to exit ...
 pause >nul
 (goto) 2>nul & del "%~f0"
