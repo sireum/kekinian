5909de4dd983ba42f955cca2ebc19f812eafe54b
Robby <robby@k-state.edu>
2019-01-25 08:33:04 -0600

    Updated distro setup scripts.

--- distro/release/setup.bat
+++ distro/release/setup.bat
@@ -1,8 +1,12 @@
 @echo off
 set INSTALL_PATH="%USERPROFILE%\Applications"
-set /p INSTALL_PATH="Enter the directory where Sireum should be installed under [Press Enter for %INSTALL_PATH%]:"
-md %INSTALL_PATH%
-move %~dp0Sireum %INSTALL_PATH%
+choice /T 5 /C YN /D N /N /M "Customize where Sireum should be installed under (defauts to %INSTALL_PATH%) [Y/N]? "
+if errorlevel 2 goto :install
+set /p INSTALL_PATH="Enter the directory path [Press Enter to use the default]: "
+:install
+md %INSTALL_PATH% 1> nul 2>&1
+rd /S /Q %INSTALL_PATH%\Sireum 2>nul
+move %~dp0Sireum %INSTALL_PATH% 1> nul 2>&1
 echo Sireum IVE can now be launched by running %INSTALL_PATH%\Sireum\bin\win\idea\bin\idea64.exe
 echo Java Development Kit (JDK) is available at %INSTALL_PATH%\Sireum\bin\win\java
 echo Scala is available at %INSTALL_PATH%\Sireum\bin\scala
