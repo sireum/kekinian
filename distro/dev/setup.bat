78e964c0fa392eeef34a08284120cd7f4f2d7aa5
Robby <robby@k-state.edu>
2019-01-24 22:12:59 -0600

    Updated distro setup scripts.

--- distro/dev/setup.bat
+++ distro/dev/setup.bat
@@ -3,6 +3,10 @@ set INSTALL_PATH="%USERPROFILE%\Applications"
 set /p INSTALL_PATH="Enter the directory where Sireum-dev should be installed under [Press Enter for %INSTALL_PATH%]:"
 md %INSTALL_PATH%
 move %~dp0Sireum-dev %INSTALL_PATH%
-Done! Press any key to exit ...
+echo Sireum-dev IVE can now be launched by running %INSTALL_PATH%\Sireum-dev\bin\win\idea\bin\idea64.exe
+echo Java Development Kit (JDK) is available at %INSTALL_PATH%\Sireum-dev\bin\win\java
+echo Scala is available at %INSTALL_PATH%\Sireum-dev\bin\scala
+echo Mill can be found at %INSTALL_PATH%\Sireum-dev\bin\mill.bat
+echo Press any key to exit ...
 pause
 (goto) 2>nul & del "%~f0"
--- distro/release/setup.bat
+++ distro/release/setup.bat
@@ -2,7 +2,11 @@
 set INSTALL_PATH="%USERPROFILE%\Applications"
 set /p INSTALL_PATH="Enter the directory where Sireum should be installed under [Press Enter for %INSTALL_PATH%]:"
 md %INSTALL_PATH%
-move %~dp0Sireum-dev %INSTALL_PATH%
-Done! Press any key to exit ...
+move %~dp0Sireum %INSTALL_PATH%
+echo Sireum IVE can now be launched by running %INSTALL_PATH%\Sireum\bin\win\idea\bin\idea64.exe
+echo Java Development Kit (JDK) is available at %INSTALL_PATH%\Sireum\bin\win\java
+echo Scala is available at %INSTALL_PATH%\Sireum\bin\scala
+echo Mill can be found at %INSTALL_PATH%\Sireum\bin\mill.bat
+echo Press any key to exit ...
 pause
 (goto) 2>nul & del "%~f0"
