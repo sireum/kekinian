bd0aa2fbc2e19d2f4fa0e551237d1ae1c8c9f552
Robby <robby@k-state.edu>
2019-06-15 22:15:06 -0500

    Added repo clean scripts.

---
+++ bin/clean.bat
@@ -0,0 +1,7 @@
+@echo off
+git reset --hard
+git submodule foreach --recursive 'git checkout -- . || :'
+git submodule update --init --recursive
+git clean -d -f -f -x
+git submodule foreach --recursive git clean -d -f -f -x
+
---
+++ bin/clean.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -e
+git reset --hard
+git submodule foreach --recursive 'git checkout -- . || :'
+git submodule update --init --recursive
+git clean -d -f -f -x
+git submodule foreach --recursive git clean -d -f -f -x
+
