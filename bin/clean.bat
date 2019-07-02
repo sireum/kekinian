ac6a29839faf4c50d8ff4e10b3858974c1f7d23a
Robby <robby@k-state.edu>
2019-07-02 11:40:29 -0600

    Updated clean.bat. [skip ci]

--- bin/clean.bat
+++ bin/clean.bat
@@ -1,7 +1,6 @@
 @echo off
 git reset --hard
-git submodule foreach --recursive 'git checkout -- . || :'
+git submodule foreach --recursive "git checkout -- . || :"
 git submodule update --init --recursive
 git clean -d -f -f -x
-git submodule foreach --recursive git clean -d -f -f -x
-
+git submodule foreach --recursive "git clean -d -f -f -x"
