25ba1ef10e83703bba88e1b74f4e6447def2c2aa
Robby <robby@k-state.edu>
2019-07-08 11:39:20 -0600

    Tweaked clean scripts. [skip ci]

--- bin/clean.sh
+++ bin/clean.sh
@@ -4,4 +4,4 @@ git submodule foreach --recursive 'git checkout -- . || :'
 git submodule update --init --recursive
 git clean -d -f -f -x
 git submodule foreach --recursive git clean -d -f -f -x
-
+git pull --recurse-submodules
