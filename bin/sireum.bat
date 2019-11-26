fe6a60c850a1863d8945c19ebffb8a4721ad3848
Robby <robby@k-state.edu>
2019-11-25 18:47:19 -0600

    Updated scripts.

--- bin/sireum.bat
+++ bin/sireum.bat
@@ -1,6 +1,7 @@
 @echo off
 setlocal
 set SIREUM_HOME=%~dp0..\
+set JAVA_HOME=%~dp0win\java
 set NEWER=False
 if exist %~dp0win\sireum.exe for /f %%i in ('powershell -noprofile -executionpolicy bypass -command "(Get-Item %~dp0win\sireum.exe).LastWriteTime -gt (Get-Item %~dp0sireum.jar).LastWriteTime"') do @set NEWER=%%i
 if "%NEWER%" == "True" goto native
@@ -8,7 +9,7 @@ del "%~dp0win\sireum.exe" > nul 2>&1
 del "%~dp0win\.sireum.exe" > nul 2>&1
 set JAVA=java.exe
 if defined SIREUM_PROVIDED_SCALA set SIREUM_PROVIDED_JAVA=true
-if not defined SIREUM_PROVIDED_JAVA set JAVA=%~dp0win\java\bin\java.exe
+if not defined SIREUM_PROVIDED_JAVA set JAVA=%JAVA_HOME%\bin\java.exe
 copy /Y "%~dp0sireum.jar" "%~dp0.sireum-win.jar" > nul 2>&1
 "%JAVA%" %JAVA_OPTS% -jar "%~dp0.sireum-win.jar" %*
 exit /B %errorlevel%
