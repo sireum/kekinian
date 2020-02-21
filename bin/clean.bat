7bd8bf3b3ec92bfe63d2ca29b20e209764d8de75
Robby <robby@k-state.edu>
2020-02-21 10:30:41 -0600

    Updated clean scripts. [skip ci]

--- bin/clean.bat
+++ bin/clean.bat
@@ -1,8 +1,7 @@
 @echo off
+rmdir /q/s %USERPROFILE%\Downloads\sireum 2> nul
 git reset --hard
 git submodule foreach --recursive "git checkout -- . || :"
 git submodule update --init --recursive
 git clean -d -f -f -x
 git submodule foreach --recursive "git clean -d -f -f -x"
-git pull --recurse-submodules
-rmdir /q/s %USERPROFILE%\Downloads\sireum 2> nul
--- bin/clean.sh
+++ bin/clean.sh
@@ -1,8 +1,7 @@
 #!/bin/sh -e
+rm -fR ~/Downloads/sireum
 git reset --hard
 git submodule foreach --recursive 'git checkout -- . || :'
 git submodule update --init --recursive
 git clean -d -f -f -x
 git submodule foreach --recursive git clean -d -f -f -x
-git pull --recurse-submodules
-rm -fR ~/Downloads/sireum
