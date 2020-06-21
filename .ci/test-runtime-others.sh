040224832fb22dee5f6d6af1496bf7a660d1eddc
Robby <robby@k-state.edu>
2020-06-21 13:44:20 -0500

    Tweaked Linux aarch64 CI scripts.

--- .ci/test-runtime-others.sh
+++ .ci/test-runtime-others.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -e
 export SIREUM_HOME=$(cd -P $(dirname "$0")/.. && pwd -P)
 cd $SIREUM_HOME
+bin/build.cmd
 bin/mill all runtime.library.shared.tests runtime.library.jvm.tests alir.shared.tests slang.parser.shared.tests tools.jvm.tests
--- .ci/test-slang-frontend.sh
+++ .ci/test-slang-frontend.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -e
 export SIREUM_HOME=$(cd -P $(dirname "$0")/.. && pwd -P)
 cd $SIREUM_HOME
+bin/build.cmd
 bin/mill slang.frontend.shared.tests
