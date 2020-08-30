4e9a8b07bb5b79ec3ba4cf8e48ac343b9ec749c1
Robby <robby@santoslab.org>
2020-08-29 19:33:02 -0500

    Simplified aarch64 native test due to Travis CI time limit.

--- .ci/linux-arm-native.sh
+++ .ci/linux-arm-native.sh
@@ -2,8 +2,11 @@
 if [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]; then
   if [ "$(uname -m)" = "aarch64" ]; then
     export SIREUM_HOME=$(cd -P $(dirname "$0")/.. && pwd -P)
-    $SIREUM_HOME/bin/build.cmd native
-    $SIREUM_HOME/bin/linux/arm/upx -9 bin/linux/arm/sireum
-    $SIREUM_HOME/bin/linux/arm/sireum
+    $SIREUM_HOME/bin/build.cmd
+    $SIREUM_HOME/bin/install/graal.cmd
+    $SIREUM_HOME/bin/build.cmd
+    # $SIREUM_HOME/bin/build.cmd native
+    # $SIREUM_HOME/bin/linux/arm/upx -9 bin/linux/arm/sireum
+    # $SIREUM_HOME/bin/linux/arm/sireum
   fi
 fi
