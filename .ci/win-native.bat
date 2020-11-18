c9a07fe9bbdb157a5a586b4a32261955f90f722b
Robby <robby@santoslab.org>
2020-11-18 11:13:46 -0600

    Updated Windows CI script.

--- .ci/win-native.bat
+++ .ci/win-native.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal
 set SIREUM_HOME=%~dp0..
-%SIREUM_HOME%\bin\build.cmd native
+call %SIREUM_HOME%\bin\build.cmd native
 %SIREUM_HOME%\bin\win\upx.exe -9 %SIREUM_HOME%\bin\win\sireum.exe
 %SIREUM_HOME%\bin\sireum
