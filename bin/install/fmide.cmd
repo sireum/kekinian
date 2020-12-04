fd50536bf19e633bb88b0607e37e4882ccac348c
Robby <robby@santoslab.org>
2020-12-04 08:38:31 -0600

    Updated fmide.cmd to update BriefCASE.

--- bin/install/fmide.cmd
+++ bin/install/fmide.cmd
@@ -21,10 +21,14 @@ exit /B %errorlevel%
 exit /B %errorlevel%
 ::!#
 // #Sireum
+
 import org.sireum._
 
 val homeBin: Os.Path = Os.slashDir.up.canon
 val home = homeBin.up.canon
+
+val briefCaseFeatureId: String = "com.collins.trustedsystems.briefcase.feature.feature.group"
+@strictpure def briefCaseUpdateSite(url: String): String = s"jar:${url}!"
 
 val eclipseRelease = "2019-12"
 val hamrUpdateSite = "https://raw.githubusercontent.com/sireum/hamr-plugin-update-site/master"
@@ -50,8 +54,10 @@ val metalsP2Args = p2Args(F, metalsUpdateSite, metalsFeatureId)
 
 val envs = ISZ[(String, String)]("PATH" ~>
   s"${Os.env("JAVA_HOME").get}${Os.fileSep}bin${Os.pathSep}${Os.env("PATH").get}")
-var platformNameUrlMap = Map.empty[Os.Kind.Type, (String, String)]
-var releaseTagNameOpt: Option[String] = None()
+var fmwPlatformNameUrlMap = Map.empty[Os.Kind.Type, (String, String)]
+var briefCaseUrlOpt: Option[String] = None()
+var fmwReleaseTagNameOpt: Option[String] = None()
+var briefCaseReleaseTagNameOpt: Option[String] = None()
 val useLast: B = T
 
 Os.cliArgs.size match {
@@ -59,46 +65,45 @@ Os.cliArgs.size match {
   case z"1" =>
     Os.cliArgs(0) match {
       case string"nightly" =>
-      case tagName => releaseTagNameOpt = Some(tagName)
+      case tagName => fmwReleaseTagNameOpt = Some(tagName)
     }
+  case z"2" =>
+    fmwReleaseTagNameOpt = Some(Os.cliArgs(0))
+    briefCaseReleaseTagNameOpt = Some(Os.cliArgs(1))
   case _ =>
-    println("Usage: fmide.cmd [<tag-name> | nightly]")
+    println("Usage: fmide.cmd [ <fmw-tag-name> [ <briefcase-tag-name> ] | nightly ]")
     Os.exit(-1)
 }
 
-def findAssets(): Unit = {
+def findAssets(repo: String): Unit = {
   var first = T
-
-  for (r <- GitHub.repo("loonwerks", "formal-methods-workbench").releases) {
-    if (releaseTagNameOpt == Some(r.tagName) || (first && releaseTagNameOpt.isEmpty)) {
-      for (a <- r.assets) {
-        val aNameOps = ops.StringOps(a.name)
-        if (aNameOps.startsWith("com.collins.trustedsystems.fmw.")) {
+  for (r <- GitHub.repo("loonwerks", repo).releases) {
+    for (a <- r.assets) {
+      val aNameOps = ops.StringOps(a.name)
+      if (aNameOps.startsWith("com.collins.trustedsystems.")) {
+        if (aNameOps.contains(".briefcase.repository-")) {
+          if (briefCaseReleaseTagNameOpt == Some(r.tagName) || (first && briefCaseReleaseTagNameOpt.isEmpty)) {
+            briefCaseUrlOpt = Some(a.url)
+            return
+          }
+        } else if (fmwReleaseTagNameOpt == Some(r.tagName) || (first && fmwReleaseTagNameOpt.isEmpty)) {
           val p = (a.name, a.url)
-          if (aNameOps.contains("win32") && (useLast || !platformNameUrlMap.contains(Os.Kind.Win))) {
-            platformNameUrlMap = platformNameUrlMap + Os.Kind.Win ~> p
-          } else if (aNameOps.contains("linux") && (useLast || !platformNameUrlMap.contains(Os.Kind.Linux))) {
-            platformNameUrlMap = platformNameUrlMap + Os.Kind.Linux ~> p
-          } else if (aNameOps.contains("macos") && (useLast || !platformNameUrlMap.contains(Os.Kind.Mac))) {
-            platformNameUrlMap = platformNameUrlMap + Os.Kind.Mac ~> p
+          if (aNameOps.contains("win32") && (useLast || !fmwPlatformNameUrlMap.contains(Os.Kind.Win))) {
+            fmwPlatformNameUrlMap = fmwPlatformNameUrlMap + Os.Kind.Win ~> p
+          } else if (aNameOps.contains("linux") && (useLast || !fmwPlatformNameUrlMap.contains(Os.Kind.Linux))) {
+            fmwPlatformNameUrlMap = fmwPlatformNameUrlMap + Os.Kind.Linux ~> p
+          } else if (aNameOps.contains("macos") && (useLast || !fmwPlatformNameUrlMap.contains(Os.Kind.Mac))) {
+            fmwPlatformNameUrlMap = fmwPlatformNameUrlMap + Os.Kind.Mac ~> p
           }
         }
       }
     }
     first = F
   }
-
-  if (platformNameUrlMap.isEmpty) {
-    releaseTagNameOpt match {
-      case Some(releaseTagName) => eprintln(s"Could not find FMIDE $releaseTagName build assets")
-      case _ => eprintln("Could not find FMIDE latest nightly build assets")
-    }
-    Os.exit(-1)
-  }
 }
 
 def download(kind: Os.Kind.Type): Os.Path = {
-  val (name, url) = platformNameUrlMap.get(kind).get
+  val (name, url) = fmwPlatformNameUrlMap.get(kind).get
   val r = Os.home / "Downloads" / "sireum" / name
   if (!r.exists) {
     println(s"Downloading $name ...")
@@ -116,13 +121,17 @@ def linux(): Unit = {
   println(s"Extracting $f ...")
   Os.proc(ISZ("tar", "xfz", f.string)).at(d).runCheck()
   println()
-  if (releaseTagNameOpt.isEmpty) {
+  val dFmide = (d / "fmide").string
+  println("Updating BriefCASE plugin ...")
+  Os.proc(dFmide +: p2Args(T, briefCaseUpdateSite(briefCaseUrlOpt.get), briefCaseFeatureId)).env(envs).runCheck()
+  println()
+  if (fmwReleaseTagNameOpt.isEmpty) {
     println(s"Updating HAMR plugin ...")
-    Os.proc((d / "fmide").string +: hamrP2Args).env(envs).runCheck()
+    Os.proc(dFmide +: hamrP2Args).env(envs).runCheck()
     println()
   }
   println(s"Installing Scala Metals plugin ...")
-  Os.proc((d / "fmide").string +: metalsP2Args).env(envs).runCheck()
+  Os.proc(dFmide +: metalsP2Args).env(envs).runCheck()
   println()
   println(s"FMIDE is installed at $d")
 }
@@ -139,13 +148,17 @@ def mac(): Unit = {
     p.moveTo(d)
   }
   println()
-  if (releaseTagNameOpt.isEmpty) {
+  val dFmide = (d / "Contents" / "MacOS" / "fmide").string
+  println("Updating BriefCASE plugin ...")
+  Os.proc(dFmide +: p2Args(T, briefCaseUpdateSite(briefCaseUrlOpt.get), briefCaseFeatureId)).env(envs).runCheck()
+  println()
+  if (fmwReleaseTagNameOpt.isEmpty) {
     println(s"Updating HAMR plugin ...")
-    Os.proc((d / "Contents" / "MacOS" / "fmide").string +: hamrP2Args).env(envs).runCheck()
+    Os.proc(dFmide +: hamrP2Args).env(envs).runCheck()
     println()
   }
   println(s"Installing Scala Metals plugin ...")
-  Os.proc((d / "Contents" / "MacOS" / "fmide").string +: metalsP2Args).env(envs).runCheck()
+  Os.proc(dFmide +: metalsP2Args).env(envs).runCheck()
   println()
   println(s"FMIDE is installed at $d")
 }
@@ -158,18 +171,38 @@ def win(): Unit = {
   println(s"Extracting $f ...")
   f.unzipTo(d)
   println()
-  if (releaseTagNameOpt.isEmpty) {
+  val dFmide = (d / "fmide").string
+  println("Updating BriefCASE plugin ...")
+  Os.proc(dFmide +: p2Args(T, briefCaseUpdateSite(briefCaseUrlOpt.get), briefCaseFeatureId)).env(envs).runCheck()
+  println()
+  if (fmwReleaseTagNameOpt.isEmpty) {
     println(s"Updating HAMR plugin ...")
-    Os.proc((d / "fmide.exe").string +: hamrP2Args).env(envs).runCheck()
+    Os.proc(dFmide +: hamrP2Args).env(envs).runCheck()
     println()
   }
   println(s"Installing Scala Metals plugin ...")
-  Os.proc((d / "fmide.exe").string +: metalsP2Args).env(envs).runCheck()
+  Os.proc(dFmide +: metalsP2Args).env(envs).runCheck()
   println()
   println(s"FMIDE is installed at $d")
 }
 
-findAssets()
+findAssets("BriefCASE")
+if (briefCaseUrlOpt.isEmpty) {
+  briefCaseReleaseTagNameOpt match {
+    case Some(releaseTagName) => eprintln(s"Could not find BriefCASE $releaseTagName build assets")
+    case _ => eprintln("Could not find BriefCASE latest nightly build assets")
+  }
+  Os.exit(-1)
+}
+
+findAssets("formal-methods-workbench")
+if (fmwPlatformNameUrlMap.isEmpty) {
+  fmwReleaseTagNameOpt match {
+    case Some(releaseTagName) => eprintln(s"Could not find FMIDE $releaseTagName build assets")
+    case _ => eprintln("Could not find FMIDE latest nightly build assets")
+  }
+  Os.exit(-1)
+}
 
 Os.kind match {
   case Os.Kind.Linux => linux()
