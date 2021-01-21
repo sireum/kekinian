eafc10cbb7f6ee8c8068802bd16eceff671de445
Robby <robby@santoslab.org>
2021-01-21 16:24:49 -0600

    Added Logika cache.

--- cli/jvm/src/main/scala/org/sireum/cli/Logika.scala
+++ cli/jvm/src/main/scala/org/sireum/cli/Logika.scala
@@ -154,8 +154,8 @@ object Logika {
       if (f.isFile && f.ext.value != ".sc") {
         val reporter = logika.Logika.Reporter.create
         logika.Logika.checkWorksheet(Some(f.value), f.read, config, (th: lang.tipe.TypeHierarchy) =>
-            logika.Smt2Impl(smt2Configs, th, config.timeoutInMs, config.charBitWidth, config.intBitWidth,
-              config.simplifiedQuery), reporter, o.par)
+            logika.Smt2Impl(smt2Configs, th,  logika.Smt2Impl.NoCache(), config.timeoutInMs, config.charBitWidth,
+              config.intBitWidth, config.simplifiedQuery), reporter, o.par)
         reporter.printMessages()
         if (reporter.hasError) {
           code = if (code == 0) ILL_FORMED_SCRIPT_FILE else code
