2bfd3e94dbfbc38e86cb7a382032e1a16dbf2bf3
Robby <robby@santoslab.org>
2021-02-23 10:40:42 -0600

    Fixed linux-arm-native.sh.

--- .ci/linux-arm-native.sh
+++ .ci/linux-arm-native.sh
@@ -6,6 +6,6 @@ if [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]; then
     $SIREUM_HOME/bin/install/graal.cmd
     $SIREUM_HOME/bin/build.cmd
     $SIREUM_HOME/bin/linux/arm/upx -9 bin/build.cmd.com
-    $SIREUM_HOME/bin/linux/arm/sireum
+    $SIREUM_HOME/bin/sireum
   fi
 fi
