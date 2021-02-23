78839501077eb71c93c2bb0cec1e6c2323bc78b6
Robby <robby@santoslab.org>
2021-02-23 06:25:10 -0600

    Upgraded dependencies.

--- bin/build.cmd
+++ bin/build.cmd
@@ -110,8 +110,8 @@ def installZ3(kind: Os.Kind.Type): Unit = {
 
   val filename: String = kind match {
     case Os.Kind.Win => s"z3-$version-x64-win.zip"
-    case Os.Kind.Linux => s"z3-$version-x64-ubuntu-16.04.zip"
-    case Os.Kind.Mac => s"z3-$version-x64-osx-10.14.6.zip"
+    case Os.Kind.Linux => s"z3-$version-x64-ubuntu-18.04.zip"
+    case Os.Kind.Mac => s"z3-$version-x64-osx-10.15.7.zip"
     case _ => return
   }
 
