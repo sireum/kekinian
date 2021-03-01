a1ef3abffe80baa88f220f96a13eb2bf44cd7d39
Robby <robby@santoslab.org>
2021-03-01 15:10:37 -0600

    Updated clean scripts to remove .mill. [skip ci].

--- bin/clean.sh
+++ bin/clean.sh
@@ -3,9 +3,9 @@ if [ -n "$COMSPEC" -a -x "$COMSPEC" ]; then
   >&2 echo "Please use clean.bat"
   exit 1
 elif [[ "$(uname)" == "Darwin" ]]; then
-  rm -fR ~/Downloads/sireum ~/Library/Application\ Support/JetBrains/SireumIVE-dev ~/Library/Caches/JetBrains/SireumIVE-dev
+  rm -fR ~/.mill ~/Downloads/sireum ~/Library/Application\ Support/JetBrains/SireumIVE-dev ~/Library/Caches/JetBrains/SireumIVE-dev
 elif [[ "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
-  rm -fR ~/Downloads/sireum ~/.SireumIVE-dev
+  rm -fR ~/.mill ~/Downloads/sireum ~/.SireumIVE-dev
 else
   >&2 echo "Sireum does not support: $(uname)."
   exit 1
