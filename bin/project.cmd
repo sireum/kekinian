f6873b2507f4f23e70124a9d3c79159ddf5617d9
Robby <robby@santoslab.org>
2021-03-30 09:33:33 -0500

    Refined project definition.

--- bin/project.cmd
+++ bin/project.cmd
@@ -1,14 +1,15 @@
-::#! 2> /dev/null                                             #
-@ 2>/dev/null # 2>nul & echo off & goto BOF                   #
-if [ -f "$0.com" ] && [ "$0.com" -nt "$0" ]; then             #
-  exec "$0.com" "$@"                                          #
-fi                                                            #
-rm -f "$0.com"                                                #
-if [ -z ${SIREUM_HOME} ]; then                                #
-  echo "Please set SIREUM_HOME env var"                       #
-  exit -1                                                     #
-fi                                                            #
-exec ${SIREUM_HOME}/bin/sireum slang run "$0" "$@"            #
+::#! 2> /dev/null                                                                          #
+@ 2>/dev/null # 2>nul & echo off & goto BOF                                                #
+BIN=$(cd -P $(dirname "$0") && pwd -P)                                                     #
+if [ -f "$0.com" ] && [ "$0.com" -nt "$0" ] && [ "$0.com" -nt "${BIN}/sireum.jar" ]; then  #
+  exec "$0.com" "$@"                                                                       #
+fi                                                                                         #
+rm -f "$0.com"                                                                             #
+if [ -z ${SIREUM_HOME} ]; then                                                             #
+  echo "Please set SIREUM_HOME env var"                                                    #
+  exit -1                                                                                  #
+fi                                                                                         #
+exec ${SIREUM_HOME}/bin/sireum slang run -n "$0" "$@"                                      #
 :BOF
 setlocal
 if not defined SIREUM_HOME (
@@ -16,10 +17,10 @@ if not defined SIREUM_HOME (
   exit /B -1
 )
 set NEWER=False
-if exist %~dpnx0.com for /f %%i in ('powershell -noprofile -executionpolicy bypass -command "(Get-Item %~dpnx0.com).LastWriteTime -gt (Get-Item %~dpnx0).LastWriteTime"') do @set NEWER=%%i
+if exist %~dpnx0.com for /f %%i in ('powershell -noprofile -executionpolicy bypass -command "((Get-Item %~dpnx0.com).LastWriteTime -gt (Get-Item %~dpnx0).LastWriteTime) -and ((Get-Item %~dpnx0.com).LastWriteTime -gt (Get-Item %~dp0\sireum.jar).LastWriteTime)"') do @set NEWER=%%i
 if "%NEWER%" == "True" goto native
 del "%~dpnx0.com" > nul 2>&1
-%SIREUM_HOME%\bin\sireum.bat slang run "%0" %*
+%SIREUM_HOME%\bin\sireum.bat slang run -n "%0" %*
 exit /B %errorlevel%
 :native
 %~dpnx0.com %*
@@ -72,12 +73,42 @@ var project = Project.empty
 
 for (p <- projects) {
   val f = p / "bin" / "project.cmd"
+  val fNative = p / "bin" / "project.cmd.com"
   if (isDot) {
     println(s"Loading $f ...")
   }
-  val proc = Os.proc(ISZ(f.string, "json"))
-  val r: Os.Proc.Result = if (isDot) proc.console.runCheck() else proc.runCheck()
-  val prj = org.sireum.project.JSON.toProject(ops.StringOps(r.out).split((c: C) => c === '\n')(0)).left
+  var prj = Project.empty
+  var ok = F
+
+  def deserialize(text: String): Unit = {
+    org.sireum.project.JSON.toProject(ops.StringOps(text).split((c: C) => c === '\n')(0)) match {
+      case Either.Left(sp) =>
+        prj = sp
+        ok = T
+      case _ =>
+    }
+  }
+
+  if (fNative.exists) {
+    if (fNative.lastModified > f.lastModified) {
+      val r = Os.proc(ISZ(fNative.string, "json")).run()
+      if (r.ok) {
+        deserialize(r.out)
+      }
+    }
+  }
+  if (!ok) {
+    fNative.removeAll()
+    val proc = Os.proc(ISZ(f.string, "json"))
+    val r: Os.Proc.Result = if (isDot) proc.console.run() else proc.run()
+    if (r.ok) {
+      deserialize(r.out)
+    }
+  }
+  if (!ok) {
+    eprintln(s"Failed loading $f ...")
+    Os.exit(-1)
+  }
   project = project ++ prj
   if (isDot) {
     println()
