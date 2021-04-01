c4adb5279c07448f69264c8bfa8df83b54bec6a6
Robby <robby@santoslab.org>
2021-04-01 08:22:07 -0500

    Set java.net.useSystemProxies to true.

--- bin/sireum.bat
+++ bin/sireum.bat
@@ -11,7 +11,7 @@ set JAVA=java.exe
 if defined SIREUM_PROVIDED_SCALA set SIREUM_PROVIDED_JAVA=true
 if not defined SIREUM_PROVIDED_JAVA set JAVA=%JAVA_HOME%\bin\java.exe
 copy /Y "%~dp0sireum.jar" "%~dp0.sireum-win.jar" > nul 2>&1
-"%JAVA%" %JAVA_OPTS% -jar "%~dp0.sireum-win.jar" %*
+"%JAVA%" -Djava.net.useSystemProxies=true %JAVA_OPTS% -jar "%~dp0.sireum-win.jar" %*
 exit /B %errorlevel%
 
 :native
