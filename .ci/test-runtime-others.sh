1a3d73d4765997fd74719ae1c6d1321d93d0c23c
Robby <robby@santoslab.org>
2021-04-07 16:00:24 -0500

    Adapted ci scripts to build.cmd changes.

--- .ci/test-runtime-others.sh
+++ .ci/test-runtime-others.sh
@@ -2,4 +2,5 @@
 export SIREUM_HOME=$(cd -P $(dirname "$0")/.. && pwd -P)
 cd $SIREUM_HOME
 bin/build.cmd
+bin/build.cmd mill
 bin/mill all runtime.library.shared.tests runtime.library.jvm.tests alir.shared.tests slang.parser.shared.tests tools.jvm.tests
--- .ci/test-slang-frontend.sh
+++ .ci/test-slang-frontend.sh
@@ -2,4 +2,5 @@
 export SIREUM_HOME=$(cd -P $(dirname "$0")/.. && pwd -P)
 cd $SIREUM_HOME
 bin/build.cmd
+bin/build.cmd mill
 bin/mill slang.frontend.shared.tests
