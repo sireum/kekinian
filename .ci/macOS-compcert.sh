00f2de551561c2405f153b59fe1aeea44edc7a63
Robby <robby@santoslab.org>
2021-06-07 08:14:52 -0500

    Added CI macOS test using CompCert. Reorganized SMT2 solvers caching.

--- .ci/macOS-compcert.sh
+++ .ci/macOS-compcert.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -e
-if [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]; then
+if [ "$(uname)" = "Darwin" ]; then
   export SIREUM_HOME=$(cd -P $(dirname "$0")/.. && pwd -P)
   cd $SIREUM_HOME
   bin/init.sh
   bin/install/compcert.cmd
-  export CC=`find $SIREUM_HOME/bin/linux -name ccomp`
+  export CC=`find $SIREUM_HOME/bin/mac -name ccomp`
   bin/sireum proyek test -n sireum-proyek --par --sha3 --packages org.sireum.transpilers.c .
 fi
