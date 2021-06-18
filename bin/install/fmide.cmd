ae1fe159d451709a028cd752fd7109145f2c2334
Robby <robby@santoslab.org>
2021-06-18 15:42:04 -0500

    Put user code at the end to avoid forward references in scripts.

--- bin/install/fmide.cmd
+++ bin/install/fmide.cmd
@@ -16,151 +16,9 @@ exit /B %errorlevel%
 // #Sireum
 // @formatter:off
 
-/*
- Copyright (c) 2017-2021, Robby, Kansas State University
- All rights reserved.
-
- Redistribution and use in source and binary forms, with or without
- modification, are permitted provided that the following conditions are met:
-
- 1. Redistributions of source code must retain the above copyright notice, this
-    list of conditions and the following disclaimer.
- 2. Redistributions in binary form must reproduce the above copyright notice,
-    this list of conditions and the following disclaimer in the documentation
-    and/or other materials provided with the distribution.
-
- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
- ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 // This file is auto-generated from fmide-cli.sc
 
 import org.sireum._
-
-// BEGIN USER CODE
-val homeBin = Os.slashDir.up.canon
-val sireum = homeBin / (if (Os.isWin) "sireum.bat" else "sireum")
-
-val fmideDir: Os.Path = Os.kind match {
-  case Os.Kind.Mac => homeBin / "mac" / "fmide.app"
-  case Os.Kind.Linux => homeBin / "linux" / "fmide"
-  case Os.Kind.Win => homeBin / "win" / "fmide"
-  case _ =>
-    eprintln("Unsupported operating system")
-    Os.exit(-1)
-    halt("Infeasible")
-}
-
-def parseCli(): (B, Cli.FmideOption) = {
-  Cli(conversions.String.toCis(Os.pathSep)(0)).parseFmide(Os.cliArgs, 0) match {
-    case Some(o: Cli.FmideOption) if o.args.size === 1 && (o.args(0) === "fixed" || o.args(0) == "latest") =>
-      return (o.args(0) === "fixed", o)
-    case Some(o: Cli.FmideOption) if o.args.isEmpty =>
-      println(o.help)
-      Os.exit(0)
-    case Some(o: Cli.HelpOption) =>
-      Os.exit(0)
-    case _ =>
-  }
-  eprintln("Could not recognize arguments")
-  Os.exit(-1)
-  halt("Infeasible")
-}
-
-val (isFixed, option) = parseCli()
-
-val compositeArtifacts = "compositeArtifacts.xml"
-
-def lookupVersion(name: String, url: String, default: String): String = {
-  def lookupVersionH(): String = {
-    if (isFixed) {
-      return default
-    }
-    val urls = ops.StringOps(url).split((c: C) => c === ',')
-    val f = Os.temp()
-    f.removeAll()
-    val durl = urls(urls.size - 1)
-    if (!f.downloadFrom(s"$durl/$compositeArtifacts")) {
-      eprintln(s"Could not find the latest version of $name; using $default")
-      return default
-    }
-    val fOps = ops.StringOps(f.read)
-    f.removeAll()
-    var i: Z = 0
-    var foundLast = F
-    val text: String = "<child location="
-    while (!foundLast) {
-      val j = fOps.stringIndexOfFrom(text, i + 1)
-      if (j < 0) {
-        foundLast = T
-      } else {
-        i = j
-      }
-    }
-    if (i === 0) {
-      eprintln(s"Could not find the latest version of $name; using $default")
-      return default
-    }
-    val offset = i + text.size + 1
-    var last = fOps.indexOfFrom('\'', offset)
-    if (last < 0) {
-      last = fOps.indexOfFrom('"', offset)
-    }
-    return fOps.substring(offset, last)
-  }
-  val r = lookupVersionH()
-  return r
-}
-
-val eclipseVersion = option.eclipse.get
-val osateVersion = option.osate.get
-
-val agreeId = "com.rockwellcollins.atc.agree.feature.feature.group"
-val agreeUrl = "https://raw.githubusercontent.com/loonwerks/AGREE-Updates/master"
-val agreeVersion = lookupVersion("AGREE", agreeUrl, option.agree.get)
-
-val resoluteId = "com.rockwellcollins.atc.resolute.feature.feature.group"
-val resoluteUrl = "https://raw.githubusercontent.com/loonwerks/Resolute-Updates/master"
-val resoluteVersion = lookupVersion("Resolute", resoluteUrl, option.resolute.get)
-
-val briefCaseId = "com.collins.trustedsystems.briefcase.feature.feature.group"
-val briefCaseUrl = s"https://download.eclipse.org/releases/$eclipseVersion,http://ca-trustedsystems-dev-us-east-1.s3-website-us-east-1.amazonaws.com/p2/snapshots/briefcase"
-val briefCaseVersion = lookupVersion("BriefCASE", briefCaseUrl, option.briefcase.get)
-
-val hamrId = "org.sireum.aadl.osate.hamr.feature.feature.group"
-val hamrUrl = "https://raw.githubusercontent.com/sireum/hamr-plugin-update-site"
-val hamrVersion: String = if (isFixed) "CASE-Tool-Assessment-4" else "master"
-
-val features = s"$hamrId=$hamrUrl/$hamrVersion;$briefCaseId=$briefCaseUrl/$briefCaseVersion;$resoluteId=$resoluteUrl/$resoluteVersion;$agreeId=$agreeUrl/$agreeVersion"
-val verContent = s"eclipse=$eclipseVersion;osate=$osateVersion;$features"
-val ver = fmideDir / "VER"
-
-if (ver.exists && ver.read == verContent) {
-  Os.exit(0)
-}
-
-ver.removeAll()
-
-fmideDir.removeAll()
-val temp = fmideDir.up.canon / s".${fmideDir.name}"
-println("Installing FMIDE ...")
-proc"$sireum hamr phantom --quiet --update --osate $temp --version $osateVersion --features $features".console.runCheck()
-for (p <- temp.list if ops.StringOps(p.name).startsWith("osate-")) {
-  p.moveTo(fmideDir)
-}
-temp.removeAll()
-
-ver.writeOver(verContent)
-println(s"FMIDE is installed at $fmideDir")
-// END USER CODE
 
 object Cli {
 
@@ -427,3 +285,120 @@ import Cli._
     }
   }
 }
+
+// BEGIN USER CODE
+val homeBin = Os.slashDir.up.canon
+val sireum = homeBin / (if (Os.isWin) "sireum.bat" else "sireum")
+
+val fmideDir: Os.Path = Os.kind match {
+  case Os.Kind.Mac => homeBin / "mac" / "fmide.app"
+  case Os.Kind.Linux => homeBin / "linux" / "fmide"
+  case Os.Kind.Win => homeBin / "win" / "fmide"
+  case _ =>
+    eprintln("Unsupported operating system")
+    Os.exit(-1)
+    halt("Infeasible")
+}
+
+def parseCliArgs(): (B, Cli.FmideOption) = {
+  Cli(Os.pathSepChar).parseFmide(Os.cliArgs, 0) match {
+    case Some(o: Cli.FmideOption) if o.args.size === 1 && (o.args(0) === "fixed" || o.args(0) == "latest") =>
+      return (o.args(0) === "fixed", o)
+    case Some(o: Cli.FmideOption) if o.args.isEmpty =>
+      println(o.help)
+      Os.exit(0)
+    case Some(o: Cli.HelpOption) =>
+      Os.exit(0)
+    case _ =>
+  }
+  eprintln("Could not recognize arguments")
+  Os.exit(-1)
+  halt("Infeasible")
+}
+
+val (isFixed, option) = parseCliArgs()
+
+def lookupVersion(name: String, url: String, default: String): String = {
+  val compositeArtifacts = "compositeArtifacts.xml"
+  def lookupVersionH(): String = {
+    if (isFixed) {
+      return default
+    }
+    val urls = ops.StringOps(url).split((c: C) => c === ',')
+    val f = Os.temp()
+    f.removeAll()
+    val durl = urls(urls.size - 1)
+    if (!f.downloadFrom(s"$durl/$compositeArtifacts")) {
+      eprintln(s"Could not find the latest version of $name; using $default")
+      return default
+    }
+    val fOps = ops.StringOps(f.read)
+    f.removeAll()
+    var i: Z = 0
+    var foundLast = F
+    val text: String = "<child location="
+    while (!foundLast) {
+      val j = fOps.stringIndexOfFrom(text, i + 1)
+      if (j < 0) {
+        foundLast = T
+      } else {
+        i = j
+      }
+    }
+    if (i === 0) {
+      eprintln(s"Could not find the latest version of $name; using $default")
+      return default
+    }
+    val offset = i + text.size + 1
+    var last = fOps.indexOfFrom('\'', offset)
+    if (last < 0) {
+      last = fOps.indexOfFrom('"', offset)
+    }
+    return fOps.substring(offset, last)
+  }
+  val r = lookupVersionH()
+  return r
+}
+
+val eclipseVersion = option.eclipse.get
+val osateVersion = option.osate.get
+
+val agreeId = "com.rockwellcollins.atc.agree.feature.feature.group"
+val agreeUrl = "https://raw.githubusercontent.com/loonwerks/AGREE-Updates/master"
+val agreeVersion = lookupVersion("AGREE", agreeUrl, option.agree.get)
+
+val resoluteId = "com.rockwellcollins.atc.resolute.feature.feature.group"
+val resoluteUrl = "https://raw.githubusercontent.com/loonwerks/Resolute-Updates/master"
+val resoluteVersion = lookupVersion("Resolute", resoluteUrl, option.resolute.get)
+
+val briefCaseId = "com.collins.trustedsystems.briefcase.feature.feature.group"
+val briefCaseUrl = s"https://download.eclipse.org/releases/$eclipseVersion,http://ca-trustedsystems-dev-us-east-1.s3-website-us-east-1.amazonaws.com/p2/snapshots/briefcase"
+val briefCaseVersion = lookupVersion("BriefCASE", briefCaseUrl, option.briefcase.get)
+
+val hamrId = "org.sireum.aadl.osate.hamr.feature.feature.group"
+val hamrUrl = "https://raw.githubusercontent.com/sireum/hamr-plugin-update-site"
+val hamrVersion: String = if (isFixed) "CASE-Tool-Assessment-4" else "master"
+
+val features = s"$hamrId=$hamrUrl/$hamrVersion;$briefCaseId=$briefCaseUrl/$briefCaseVersion;$resoluteId=$resoluteUrl/$resoluteVersion;$agreeId=$agreeUrl/$agreeVersion"
+val verContent = s"eclipse=$eclipseVersion;osate=$osateVersion;$features"
+val ver = fmideDir / "VER"
+
+if (ver.exists && ver.read == verContent) {
+  Os.exit(0)
+}
+
+ver.removeAll()
+
+fmideDir.removeAll()
+val temp = fmideDir.up.canon / s".${fmideDir.name}"
+temp.removeAll()
+println("Installing FMIDE ...")
+proc"$sireum hamr phantom --quiet --update --osate $temp --version $osateVersion --features $features".console.runCheck()
+for (p <- temp.list if ops.StringOps(p.name).startsWith("osate-")) {
+  p.moveTo(fmideDir)
+}
+temp.removeAll()
+
+ver.writeOver(verContent)
+println(s"FMIDE is installed at $fmideDir")
+// END USER CODE
