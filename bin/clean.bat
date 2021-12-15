895b3ec05050428b4032827dfe666801767938e6
Robby <robby@santoslab.org>
2021-12-15 12:58:37 -0600

    Updated clean scripts. [skip ci]

--- bin/clean.bat
+++ bin/clean.bat
@@ -3,6 +3,7 @@ rmdir /q/s %USERPROFILE%\Downloads\sireum 2> nul
 rmdir /q/s %USERPROFILE%\.SireumIVE-dev 2> nul
 rmdir /q/s %USERPROFILE%\.SireumIVE-ult-dev 2> nul
 rmdir /q/s %USERPROFILE%\.mill 2> nul
+rmdir /q/s %USERPROFILE%\.m2\repository\org\sireum\kekinian 2> nul
 git reset --hard
 git submodule foreach --recursive "git checkout -- . || :"
 git submodule update --init --recursive
--- bin/clean.sh
+++ bin/clean.sh
@@ -3,9 +3,9 @@ if [ -n "$COMSPEC" -a -x "$COMSPEC" ]; then
   >&2 echo "Please use clean.bat"
   exit 1
 elif [[ "$(uname)" == "Darwin" ]]; then
-  rm -fR ~/.mill ~/Downloads/sireum ~/Library/Application\ Support/JetBrains/SireumIVE-dev ~/Library/Caches/JetBrains/SireumIVE-dev ~/Library/Application\ Support/JetBrains/SireumIVE-ult-dev ~/Library/Caches/JetBrains/SireumIVE-ult-dev
+  rm -fR ~/.mill ~/Downloads/sireum ~/.m2/repository/org/sireum/kekinian ~/Library/Application\ Support/JetBrains/SireumIVE-dev ~/Library/Caches/JetBrains/SireumIVE-dev ~/Library/Application\ Support/JetBrains/SireumIVE-ult-dev ~/Library/Caches/JetBrains/SireumIVE-ult-dev
 elif [[ "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
-  rm -fR ~/.mill ~/Downloads/sireum ~/.SireumIVE-dev ~/.SireumIVE-ult-dev
+  rm -fR ~/.mill ~/Downloads/sireum ~/.m2/repository/org/sireum/kekinian ~/.SireumIVE-dev ~/.SireumIVE-ult-dev
 else
   >&2 echo "Sireum does not support: $(uname)."
   exit 1
