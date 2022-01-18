13f3069d639a35e2344798a7151683084e9afe90
Robby <robby@santoslab.org>
2022-01-18 10:06:21 -0600

    Upgraded projector server.

--- bin/install/projector-server.cmd
+++ bin/install/projector-server.cmd
@@ -11,7 +11,7 @@ exit /B %errorlevel%
 // #Sireum
 import org.sireum._
 
-val version = "1.5.0"
+val version = "1.6.0"
 
 if (Os.kind != Os.Kind.Linux) {
   eprintln("This feature is only available on Linux amd64")
