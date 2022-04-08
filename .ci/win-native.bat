27c9b6ff4d9ec4d5c63d03f6ed8a478bf56c68a6
Robby <robby@santoslab.org>
2022-04-08 08:52:45 -0500

    Tweaked Windows CI.

--- .ci/win-native.bat
+++ .ci/win-native.bat
@@ -4,7 +4,7 @@ set SIREUM_HOME=%~dp0..
 set errorlevel=
 call %SIREUM_HOME%\bin\build.cmd native || goto :error
 REM %SIREUM_HOME%\bin\win\upx.exe -9 %SIREUM_HOME%\bin\win\sireum.exe || goto :error
-call %SIREUM_HOME%\bin\sireum || goto :error
+REM call %SIREUM_HOME%\bin\sireum || goto :error
 goto :EOF
 :error
 exit /b %errorlevel%
