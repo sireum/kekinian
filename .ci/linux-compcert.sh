2ba4284e55f77856f3e0afea668ae5c0aa3ecbbd
Robby <robby@santoslab.org>
2022-05-06 10:14:55 -0500

    Build Sireum before running transpiler tests using CompCert.

--- .ci/linux-compcert.sh
+++ .ci/linux-compcert.sh
@@ -2,7 +2,7 @@
 if [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]; then
   export SIREUM_HOME=$(cd -P $(dirname "$0")/.. && pwd -P)
   cd $SIREUM_HOME
-  bin/init.sh
+  bin/build.cmd
   bin/install/compcert.cmd
   export CC=`find $SIREUM_HOME/bin/linux -name ccomp`
   bin/sireum proyek test -n sireum-proyek --par --sha3 --packages org.sireum.transpilers.c .
