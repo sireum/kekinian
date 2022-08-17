a5730cfbd00886e070d999f9baaa88a2137a9537
Robby <robby@santoslab.org>
2022-08-17 14:57:42 -0500

    Updated Graal image-native options.

--- cli/jvm/src/main/scala/org/sireum/cli/SlangRunner.scala
+++ cli/jvm/src/main/scala/org/sireum/cli/SlangRunner.scala
@@ -170,6 +170,7 @@ object SlangRunner {
       command = (nativeImage.string +: flags) ++ ISZ("--initialize-at-build-time", "--allow-incomplete-classpath",
         "--report-unsupported-elements-at-runtime", "--no-fallback", "-H:+ReportExceptionStackTraces",
         "-H:-DeadlockWatchdogExitOnTimeout", "-H:DeadlockWatchdogInterval=0", "--enable-url-protocols=https",
+        "--trace-object-instantiation=java.util.concurrent.ForkJoinWorkerThread", "--diagnostics-mode",
         "-cp", sJar.string, "-jar", jarFile.name, nativeName)
       r = Os.proc(command).at(jarFile.up).redirectErr.run()
       for (f <- wd.list if ops.StringOps(f.name).startsWith(s"$nativeName.") && !ops.StringOps(f.name).endsWith(".exe")) {
