2a77e5bfa90ca15dea98a500ab51d43f4f6f52aa
Robby <robby@santoslab.org>
2022-10-05 14:56:14 -0500

    Upgraded deps.

--- bin/install/clion.cmd
+++ bin/install/clion.cmd
@@ -28,7 +28,7 @@ val url = s"https://download.jetbrains.com/cpp"
 
 val homeBin = Os.slashDir.up.canon
 val home = homeBin.up.canon
-val clionVersion = "2022.2.2"
+val clionVersion = "2022.2.3"
 val sireumJar = homeBin / "sireum.jar"
 val sireum = homeBin / (if (Os.isWin) "sireum.bat" else "sireum")
 val versions = (home / "versions.properties").properties
