3dcd426e1ee522f76c95458b190f691ef73a2281
Jason Belt <belt@ksu.edu>
2023-04-05 14:54:20 -0500

    update osate

--- cli/jvm/src/main/scala/org/sireum/Cli.scala
+++ cli/jvm/src/main/scala/org/sireum/Cli.scala
@@ -1288,7 +1288,7 @@ import Cli._
           |                           Sireum plugins installed if not provided (expects a
           |                           string separated by ";")
           |-v, --version            OSATE version (expects a string; default is
-          |                           "2.11.0-vfinal")""".render
+          |                           "2.12.0-vfinal")""".render
 
     var impl: Option[String] = None[String]()
     var main: Option[String] = None[String]()
@@ -1300,7 +1300,7 @@ import Cli._
     var osate: Option[String] = None[String]()
     var update: B = false
     var features: ISZ[String] = ISZ[String]()
-    var version: Option[String] = Some("2.11.0-vfinal")
+    var version: Option[String] = Some("2.12.0-vfinal")
     var j = i
     var isOption = T
     while (j < args.size && isOption) {
