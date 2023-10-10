efe4d0cfcbeaf59e9e0ab3f36f7cc7fcd2d018e3
Jason Belt <belt@ksu.edu>
2023-10-10 14:18:15 -0500

    use phantom to generate AIR during HAMR + Slangcheck tests
    * use phantom to generate AIR
    
    * switch to osate, base cache on codegen resource
    
    * update bin distros
    
    * use sireum.bat when on windows

--- cli/jvm/src/test/scala/org/sireum/cli/slangcheck/SlangCheckHAMRIntegrationTest.scala
+++ cli/jvm/src/test/scala/org/sireum/cli/slangcheck/SlangCheckHAMRIntegrationTest.scala
@@ -1,5 +1,6 @@
 package org.sireum.cli.slangcheck
 
+import org.scalatest.BeforeAndAfterAll
 import org.sireum._
 import org.sireum.message.Reporter
 import org.sireum.test.TestSuite
@@ -7,7 +8,7 @@ import org.sireum.tools.slangcheck.TestUtil
 
 import java.io.{ByteArrayOutputStream, PrintStream}
 
-class SlangCheckHAMRIntegrationTest extends TestSuite with TestUtil {
+class SlangCheckHAMRIntegrationTest extends TestSuite with TestUtil with BeforeAndAfterAll {
   val resourceDir: Os.Path = Os.path(implicitly[sourcecode.File].value).up.up.up.up.up.up.up.up.up.up / "tools" / "jvm" / "src" / "test" / "resources" / "org" / "sireum" / "tools" / "slangcheck" / "hamr"
 
   "isolette" in {
@@ -37,18 +38,17 @@ class SlangCheckHAMRIntegrationTest extends TestSuite with TestUtil {
       System.setOut(new PrintStream(outCapture))
       System.setErr(new PrintStream(errCapture))
 
-      val json = (resourceDir / projName / "aadl" / ".slang").list.filter(p => p.ext == string"json")
-      assert(json.size == 1)
-
       val proyekRootDir = resultsDir / "hamr" / "slang"
 
       var reporter = Reporter.create
 
-      def check(tool:String, exitCode: Z): Unit = {
+      def check(tool: String, exitCode: Z): Unit = {
         if (verbose || exitCode != 0 || reporter.hasError) {
           reporter.printMessages()
-          oldOut.println(s"Out: \n${outCapture.toString}"); oldOut.flush()
-          oldErr.println(s"Err: \n${errCapture.toString}"); oldErr.flush()
+          oldOut.println(s"Out: \n${outCapture.toString}");
+          oldOut.flush()
+          oldErr.println(s"Err: \n${errCapture.toString}");
+          oldErr.flush()
         }
         assert(exitCode == 0 && !reporter.hasError, s"$tool failed")
 
@@ -56,6 +56,18 @@ class SlangCheckHAMRIntegrationTest extends TestSuite with TestUtil {
         outCapture.reset()
         errCapture.reset()
       }
+
+      val aadlDir = resourceDir / projName / "aadl"
+
+      oldOut.println(s"Regenerating AIR/JSON via Phantom using ${osateHome.get} ...")
+      (aadlDir / ".slang").removeAll()
+      val ocmd = ISZ[String]("hamr", "phantom", "-o", osateHome.get.value, aadlDir.value)
+      val oresult = Sireum.run(ocmd, reporter)
+
+      check("Phantom", oresult)
+
+      val json = (aadlDir / ".slang").list.filter(p => p.ext == string"json")
+      assert(json.size == 1, s"Expecting a single AIR/JSON file but found ${json.size}")
 
       oldOut.println("Running codegen which invokes SlangCheck via a callback ...")
       val hcmd = ISZ[String]("hamr", "codegen", "--no-proyek-ive", "--package-name", packageName, "--output-dir", proyekRootDir.value, json(0).value)
@@ -80,6 +92,58 @@ class SlangCheckHAMRIntegrationTest extends TestSuite with TestUtil {
     finally {
       System.setErr(oldErr)
       System.setOut(oldOut)
+    }
+  }
+
+  var osateHome: Option[Os.Path] = None()
+
+  override def beforeAll(): Unit = {
+    if (!willingToWait) {
+      return
+    }
+    Os.env("SIREUM_HOME") match {
+      case Some(h) =>
+        val sireumHome = Os.path(h)
+        val pv = "phantom_versions.properties"
+        val loc: (Option[Os.Path], ISZ[String]) =
+          if (Os.isWin) (Some(sireumHome / "bin" / "win" / "osate"), ISZ(pv))
+          else if (Os.isMac) (Some(sireumHome / "bin" / "mac" / "osate.app"), ISZ("Contents", "Eclipse", pv))
+          else if (Os.isLinux) (Some(sireumHome / "bin" / "linux" / "osate"), ISZ(pv))
+          else (None(), ISZ())
+
+        if (loc._1.isEmpty) {
+          assert(F, s"Unsupported operating system: ${Os.kind}")
+          halt("Infeasible assuming -ea")
+        }
+        osateHome = loc._1
+
+        val phantomVer = Os.path(implicitly[sourcecode.File].value).up.up.up.up.up.up.up.up.up.up / "hamr" / "codegen" / "jvm" / "src" / "main" / "resources" / pv
+        val cachedVer = osateHome.get /+ loc._2
+
+        def iu(installing: B): Unit = {
+          println(s"${if (installing) "Installing" else "Updating"} OSATE ${if(installing) "to" else "at" } ${osateHome.get} (this will take a while) ...")
+          val sireum = sireumHome / "bin" / (if (Os.isWin) "sireum.bat" else "sireum")
+          val oresults = proc"$sireum hamr phantom -u -o ${osateHome.get}".at(sireumHome).run()
+          if (oresults.ok) {
+            cachedVer.writeOver(phantomVer.read)
+          } else {
+            println(oresults.out)
+            println(oresults.err)
+            assert(F, s"OSATE ${if (installing) "installation" else "update"} failed")
+          }
+        }
+
+        if (!osateHome.get.exists) {
+          iu(T)
+        } else if (phantomVer.read != cachedVer.read) {
+          iu(F)
+        } else {
+          println("OSATE up-to-date")
+        }
+
+      case _ =>
+        assert(F, "Please set the SIREUM_HOME environment variable")
+        halt("Infeasible assuming -ea")
     }
   }
 }
