b4e14afa5b7b173237da3847b2901f63bb3b0882
Robby <robby@santoslab.org>
2024-10-26 09:56:37 -0500

    Updated win-native.bat to build jar first before building native.

--- .ci/win-native.bat
+++ .ci/win-native.bat
@@ -2,6 +2,8 @@
 setlocal
 set SIREUM_HOME=%~dp0..
 set errorlevel=
+call %SIREUM_HOME%\bin\build.cmd || goto :error
+del %SIREUM_HOME%\bin\.sireum-win.jar || goto :error
 call %SIREUM_HOME%\bin\build.cmd native || goto :error
 call %SIREUM_HOME%\bin\build.cmd -h || goto :error
 call %SIREUM_HOME%\bin\sireum.bat || goto :error
